b22061713b4c1127c160b22933974c65be641e96
Amal Majeed <amalmajeed7@gmail.com>
2022-06-03 11:48:48 -0600

    Added basic timer for 20 seconds

---
+++ timer.js
@@ -0,0 +1,20 @@
+// var dom = document;
+var startButton = document.querySelector("#startbutton");
+var timerDisplay = document.querySelector("#clock");
+var duration = 20;
+
+// Function that triggers the count-down and updates the clock display in the html
+const myTimer = async (time, clock) => {
+  for (let i = time; i >= 0; --i) {
+    await new Promise((resolve, reject) => {
+      setTimeout(resolve, 1000);
+
+      clock.innerHTML = `00:${String(i).padStart(2, "0")}`;
+    });
+  }
+  clock.innerHTML = "00:00"; // Need to add reset functionality
+};
+
+startButton.addEventListener("click", () => {
+  myTimer(this.duration, this.timerDisplay);
+});
