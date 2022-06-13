49af41de09524634ec3d7bffc80cdfde1a5b30d2
Amal Majeed <amalmajeed7@gmail.com>
2022-06-13 12:44:14 -0600

    Added countdown audio samples for both times

--- timer.js
+++ timer.js
@@ -1,42 +1,18 @@
 var startButton = document.querySelector("#startbutton");
 var timerDisplay1 = document.querySelector("#focusclock");
 var timerDisplay2 = document.querySelector("#breakclock");
-var focusMinutes = 25;
+var focusMinutes = 24;
 var breakMinutes = 5;
 var btnDisable = false;
-
-const breakTimer = async (time, clock) => {
-  setTimeout(() => {}, 1000);
-  for (let i = time - 1; i >= 0; --i) {
-    let j = 59;
-    if (!btnDisable) {
-      break;
-    }
-    while (j % 60 >= 0) {
-      if (!btnDisable) {
-        break;
-      }
-      await new Promise((resolve, reject) => {
-        // The padstart converts the seconds to XX double digit format with default pad value as '0'
-        clock.innerHTML = `${String(i).padStart(2, "0")}:${String(j).padStart(
-          2,
-          "0"
-        )}`;
-        setTimeout(resolve, 1000);
-        j = j - 1;
-      });
-    }
-  }
-  btnDisable = false;
-  clock.innerHTML = "25:00"; // Need to add reset functionality
-};
 
 // Function that triggers the count-down and updates the clock display in the html
 
 const myTimer = async (time, clock, t, c) => {
-  // var localMinutes = time-1;
+  var audio = new Audio("./static/mixkit-female-microphone-countdown-341.wav");
+  // audio.play();
   let rounds = 3;
   for (let r = 1; r <= rounds; ++r) {
+    btnDisable = true;
     let roundMessage = document.querySelector("#roundcount");
     let innert = t;
     roundMessage.innerHTML = `<h3>Ready for Round ${r}</h3>`;
@@ -54,6 +30,9 @@ const myTimer = async (time, clock, t, c) => {
         }
         await new Promise((resolve, reject) => {
           // The padstart converts the seconds to XX double digit format with default pad value as '0'
+          if (j == 10) {
+            audio.play();
+          }
           clock.innerHTML = `${String(i).padStart(2, "0")}:${String(j).padStart(
             2,
             "0"
@@ -75,6 +54,9 @@ const myTimer = async (time, clock, t, c) => {
         }
         await new Promise((resolve, reject) => {
           // The padstart converts the seconds to XX double digit format with default pad value as '0'
+          if (j == 10) {
+            audio.play();
+          }
           c.innerHTML = `${String(innert).padStart(2, "0")}:${String(
             j
           ).padStart(2, "0")}`;
@@ -87,6 +69,9 @@ const myTimer = async (time, clock, t, c) => {
     btnDisable = false;
     clock.innerHTML = "25:00"; // Need to add reset functionality
     c.innerHTML = "05:00";
+    startButton.classList.add("btn-success");
+    startButton.classList.remove("btn-danger");
+    startButton.innerHTML = "Start";
   }
 };
 
