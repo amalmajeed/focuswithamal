4c9891b0c75b29d473fda2f68e5c4be56c1c2e30
Amal Majeed <amalmajeed7@gmail.com>
2022-06-12 22:47:30 -0600

    Timer reset logic

--- timer.js
+++ timer.js
@@ -3,30 +3,21 @@ var timerDisplay = document.querySelector("#clock");
 var minutes = 45;
 var btnDisable = false;
 
-// const myTimer = (time, clock) => {
-//   setTimeout(() => {}, 1000);
-//   for (let i = time - 1; i >= 0; --i) {
-//     let j = 59;
-//     while (j % 60 >= 0) {
-//       setTimeout(() => {}, 1000);
-//       // The padstart converts the seconds to XX double digit format with default pad value as '0'
-//       clock.innerHTML = `${String(i).padStart(2, "0")}:${String(j).padStart(
-//         2,
-//         "0"
-//       )}`;
-//       j = j - 1;
-//     }
-//   }
-//   btnDisable = false;
-//   clock.innerHTML = "45:00"; // Need to add reset functionality
-// };
+var StrtBtn = `<button id="startbutton" class="btn btn-success">Start</button>`;
+var ResetBtn = `<button id="resetbutton" class="btn btn-danger">Reset</button>`;
 
 // Function that triggers the count-down and updates the clock display in the html
 const myTimer = async (time, clock) => {
   setTimeout(() => {}, 1000);
   for (let i = time - 1; i >= 0; --i) {
     let j = 59;
+    if (!btnDisable) {
+      break;
+    }
     while (j % 60 >= 0) {
+      if (!btnDisable) {
+        break;
+      }
       await new Promise((resolve, reject) => {
         // The padstart converts the seconds to XX double digit format with default pad value as '0'
         clock.innerHTML = `${String(i).padStart(2, "0")}:${String(j).padStart(
@@ -45,7 +36,16 @@ const myTimer = async (time, clock) => {
 startButton.addEventListener("click", () => {
   if (!this.btnDisable) {
     btnDisable = true;
+    this.startButton.innerHTML = "Reset";
+    this.startButton.classList.add("btn-danger");
+    this.startButton.classList.remove("btn-success");
     myTimer(this.minutes, this.timerDisplay);
+  } else {
+    btnDisable = false;
+    this.startButton.innerHTML = "Start";
+    this.startButton.classList.add("btn-success");
+    this.startButton.classList.remove("btn-danger");
+    this.timerDisplay.innerHTML = `45:00`;
   }
 });
 
