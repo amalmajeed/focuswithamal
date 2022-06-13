ba3a30ec164c8dbc3b158926dee408365c0f537f
Amal Majeed <amalmajeed7@gmail.com>
2022-06-12 19:25:30 -0600

    Styling the timers

--- timer.js
+++ timer.js
@@ -1,20 +1,31 @@
-// var dom = document;
-var startButton = document.querySelector("#startbutton");
-var timerDisplay = document.querySelector("#clock");
-var duration = 20;
+function MainLogic() {
+  var startButton = document.querySelector("#startbutton");
+  var timerDisplay = document.querySelector("#clock");
+  var minutes = 45;
+  var btnDisable = false;
 
-// Function that triggers the count-down and updates the clock display in the html
-const myTimer = async (time, clock) => {
-  for (let i = time; i >= 0; --i) {
-    await new Promise((resolve, reject) => {
-      setTimeout(resolve, 1000);
-      // The padstart converts the seconds to XX double digit format with default pad value as '0'
-      clock.innerHTML = `00:${String(i).padStart(2, "0")}`;
-    });
-  }
-  clock.innerHTML = "00:00"; // Need to add reset functionality
-};
+  // Function that triggers the count-down and updates the clock display in the html
+  const myTimer = async (time, clock) => {
+    for (let i = time; i >= 0; --i) {
+      await new Promise((resolve, reject) => {
+        setTimeout(resolve, 1000);
+        // The padstart converts the seconds to XX double digit format with default pad value as '0'
+        clock.innerHTML = `${String(i).padStart(2, "0")}:${String(i).padStart(
+          2,
+          "0"
+        )}`;
+      });
+    }
+    btnDisable = false;
+    clock.innerHTML = "00:00"; // Need to add reset functionality
+  };
 
-startButton.addEventListener("click", () => {
-  myTimer(this.duration, this.timerDisplay);
-});
+  startButton.addEventListener("click", () => {
+    if (!this.btnDisable) {
+      this.btnDisable = true;
+      myTimer(minutes, timerDisplay);
+    }
+  });
+}
+
+window.onload = MainLogic;
